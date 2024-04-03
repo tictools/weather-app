166e4c0491d0c8c57f13175246470abe1d08eb6f
tictools <jaumevillarreal@gmail.com>
2024-04-03 19:28:18 +0200

    feat: extract mock data for dev purpose

---
+++ src/mocks/mockWeather.js
@@ -0,0 +1,96 @@
+export const weather = {
+  lastUpdated: "2024-04-02 10:30",
+  temperature: {
+    celsius: {
+      rawValue: 8,
+      formattedValue: "8°C",
+    },
+    fahrenheit: {
+      rawValue: 8,
+      formattedValue: "46.4°F",
+    },
+  },
+  feelsLike: {
+    celsius: {
+      rawValue: 4.1,
+      formattedValue: "4.1°C",
+    },
+    fahrenheit: {
+      rawValue: 39.4,
+      formattedValue: "39.4°F",
+    },
+  },
+  isDay: true,
+  condition: {
+    text: "Sunny",
+    icon: "//cdn.weatherapi.com/weather/64x64/day/113.png",
+    code: 1000,
+  },
+  wind: {
+    metric: {
+      speed: {
+        rawValue: 3.6,
+        formattedValue: "3.6/kph",
+      },
+      gust: {
+        rawValue: 32.3,
+        formattedValue: "32.3/kph",
+      },
+    },
+    imperial: {
+      speed: {
+        rawValue: 2.2,
+        formattedValue: "32.3/mph",
+      },
+      gust: {
+        rawValue: 20.1,
+        formattedValue: "20.1/mph",
+      },
+    },
+    degree: {
+      rawValue: 136,
+      formattedValue: "136°",
+    },
+    direction: "SE",
+  },
+  pressure: {
+    metric: {
+      rawValue: 1019,
+      formattedValue: "1019mb",
+    },
+    imperial: {
+      rawValue: 30.09,
+      formattedValue: "30.09in",
+    },
+  },
+  precipitation: {
+    metric: {
+      rawValue: 0,
+      formattedValue: "0mm",
+    },
+    imperial: {
+      rawValue: 0,
+      formattedValue: "0in",
+    },
+  },
+  visibility: {
+    metric: {
+      rawValue: 10,
+      formattedValue: "10km",
+    },
+    imperial: {
+      rawValue: 6,
+      formattedValue: "6miles",
+    },
+  },
+  humidity: {
+    rawValue: 71,
+    formattedValue: "71%",
+  },
+  cloud: {
+    rawValue: 0,
+  },
+  uv: {
+    rawValue: 4,
+  },
+};
