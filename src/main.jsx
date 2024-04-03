2e8623d35bcd41fe8566e82df976df3707a2ce18
tictools <jaumevillarreal@gmail.com>
2024-04-03 10:57:01 +0200

    feat: remove fake data and strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
@@ -6,8 +5,4 @@ import "@fontsource-variable/outfit";
 import "@fontsource-variable/roboto-flex";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
